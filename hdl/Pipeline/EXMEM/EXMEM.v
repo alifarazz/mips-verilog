6b091e581d8b55a5e02b9fe9b62e733c6d6d6f11
luce <alifarazdaghi@gmail.com>
2018-12-31 11:13:37 +0330

    Fix EXMEM wiring

--- hdl/Pipeline/EXMEM/EXMEM.v
+++ hdl/Pipeline/EXMEM/EXMEM.v
@@ -26,8 +26,7 @@ module EXMEM(i_ctlwb,
    input ialu_zero;
    input [31:0] iadder_output,
                 ialu_output,
-                iread_dat_2,
-                isgn_extended;
+                iread_dat_2;
    input [4:0] imux_out;
 
    output reg [1:0] o_ctlwb;
@@ -35,8 +34,7 @@ module EXMEM(i_ctlwb,
    output reg oalu_zero;
    output reg [31:0] oadder_output,
                 oalu_output,
-                oread_dat_2,
-                osgn_extended;
+                oread_dat_2;
    output reg [4:0] omux_out;
 
    input clk, hit, rstn;
@@ -48,19 +46,17 @@ module EXMEM(i_ctlwb,
       oadder_output = 0;
       oalu_output = 0;
       oread_dat_2 = 0;
-      osgn_extended = 0;
       omux_out = 0;
    end
 
    always @(negedge clk) begin
       if (hit) begin
-         o_ctlwb = i_ctlmem;
+         o_ctlwb = i_ctlwb;
          o_ctlmem = i_ctlmem;
          oalu_zero = ialu_zero;
          oadder_output = iadder_output;
          oalu_output = ialu_output;
          oread_dat_2 = iread_dat_2;
-         osgn_extended = isgn_extended;
          omux_out = imux_out;
       end
    end
